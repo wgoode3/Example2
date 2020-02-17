8fdaf40861bdc7f95f7ae05306bd8265c2fb978a
William Goode <wgoode3@gmail.com>
2020-02-17 14:05:55 -0500

    initial

---
+++ algos.js
@@ -0,0 +1,11 @@
+function test(arr) {
+    var s = 0;
+    for(var i=0; i<arr.length; i++) {
+        s += arr[i];
+        console.log(s);
+    }
+    return s;
+}
+
+var result = test( [1,2,3] );
+console.log(result);
